aa2a2d3936290c368ff1c8b32b3d75de63740922
zhonghao <593513235@qq.com>
2019-09-29 17:52:29 +0800

    解决综合汇总update不更新的问题

--- youyaboot-service/src/main/java/com/magicalcoder/youyaboot/service/sumspecial/service/impl/SumSpecialServiceImpl.java
+++ youyaboot-service/src/main/java/com/magicalcoder/youyaboot/service/sumspecial/service/impl/SumSpecialServiceImpl.java
@@ -1,25 +1,20 @@
 package com.magicalcoder.youyaboot.service.sumspecial.service.impl;
 
-import com.magicalcoder.youyaboot.model.CommonSum;
+import com.magicalcoder.youyaboot.core.service.CommonServiceImpl;
+import com.magicalcoder.youyaboot.core.utils.StringUtil;
 import com.magicalcoder.youyaboot.model.SpecialSum;
 import com.magicalcoder.youyaboot.service.score.mapper.ScoreMapper;
 import com.magicalcoder.youyaboot.service.sumspecial.mapper.SumSpecialMapper;
 import com.magicalcoder.youyaboot.service.sumspecial.service.SumSpecialService;
-import com.magicalcoder.youyaboot.model.SumSpecial;
-import com.magicalcoder.youyaboot.core.service.CommonServiceImpl;
-import com.magicalcoder.youyaboot.core.utils.StringUtil;
-
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.stereotype.Service;
-import com.magicalcoder.youyaboot.core.utils.CopyUtil;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
-import java.util.*;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.math.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 /**
 * 代码为自动生成 Created by www.magicalcoder.com
 * 软件作者：何栋宇 qq:709876443
